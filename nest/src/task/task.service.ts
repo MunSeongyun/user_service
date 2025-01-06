4e39c97b32fa492b33c80d2d59254c8c9f45db9d
MunSeongyun <borygashill608@gmail.com>
2025-01-06 15:20:46 +0900

    asd

--- nest/src/task/task.service.ts
+++ nest/src/task/task.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { Cron, Interval } from '@nestjs/schedule';
+import { Cron, Interval, SchedulerRegistry, Timeout } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 
 @Injectable()
 export class TaskService {
+    constructor(
+        private schedulerRegistry:SchedulerRegistry
+    ){
+        this.addCronJob()
+    }
+
+    addCronJob(){
+        const name = 'cronSample'
+        const job = new CronJob('* * * * * *',()=>{
+            console.error(`run ${name}`)
+        })
+        this.schedulerRegistry.addCronJob(name, job)
+        console.warn(`job ${name} added!`)
+    }
+
     @Cron('* * * * * *',{name:'cronTask'})
     handleCron(){
         console.log('Task called')
@@ -11,5 +27,10 @@ export class TaskService {
     @Interval('intervalTask',3000)
     handleInterval(){
         console.log('Interval called')
+    }
+
+    @Timeout('timeoutTask',5000)
+    handelTimeout(){
+        console.log('Timeout called')
     }
 }
